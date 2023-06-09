01003f878dae07a23af670ba3d35b71c4b00fe3f
Michał Wdowski <michal.wdowski99@gmail.com>
2023-06-09 16:06:08 +0200

    WIP

--- src/components/HomeOrganizer/HomeOrganizer.js
+++ src/components/HomeOrganizer/HomeOrganizer.js
@@ -1,11 +1,104 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getToken, LogOut, getUserID } from './../../parseToken'
 
 const HomeOrganizer = (props) => {
-    
-    return(
-        <div>
-            JESTEM W DOMU ORGANIZATORA!!!!
-        </div>
+
+    if (!getToken()) {
+        props.setToken(null);
+        window.location.reload();
+    }
+
+    const [organizerData, setOrganizerData] = useState(null);
+    const [eventsData, setEventsData] = useState(null);
+
+    const getOrganizerData = async (e) => {
+        const id = getUserID();
+        const token = getToken();
+
+        if (!token || !id) {
+            LogOut()
+            props.setToken(null);
+            window.location.reload();
+        }
+
+        await fetch("https://kgp-ticketapp.azurewebsites.net/users/organizers/" + id, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token.toString()
+            }
+        }).then(async (response) => {
+
+            if (response.status === 200) {
+                const body = await response.json()
+                console.log(body);
+                setOrganizerData(body);
+            }
+            else {
+                console.log("Jakiś błąd :(");
+            }
+
+        }).catch((error) => {
+            console.log("Jakiś błąd tylko gorszy :(");
+        });
+    };
+
+    const getEventsData = async (e) => {
+        const id = getUserID();
+        const token = getToken();
+
+        if (!token || !id) {
+            LogOut()
+            props.setToken(null);
+            window.location.reload();
+        }
+
+        await fetch("https://kgp-ticketapp.azurewebsites.net/eventsByOrganizer/" + id, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token.toString()
+            }
+        }).then(async (response) => {
+
+            if (response.status === 200) {
+                const body = await response.json()
+                console.log(body);
+                setEventsData(body);
+            }
+            else {
+                console.log("Jakiś błąd :(");
+            }
+
+        }).catch((error) => {
+            console.log("Jakiś błąd tylko gorszy :(");
+        });
+    };
+
+    useEffect(() => {
+        getOrganizerData();
+        getEventsData();
+    }, [])
+
+    const renderEventsList = () => {
+        let tr = [];
+        if (eventsData !== null) {
+            for (let event of eventsData) {
+                tr.push(<tr>{event.name}</tr>);
+            }
+        }
+        return tr;
+    }
+
+    return (
+        <>
+            <h1>Hello {organizerData?.companyName}</h1>
+            <h2>Your events:</h2>
+            <table>
+                {renderEventsList()}
+            </table>
+        </>
+
     )
 }
 
--- src/parseToken.js
+++ src/parseToken.js
@@ -1,8 +1,7 @@
 
-export function check_cookie_name(name) 
-{
+export function check_cookie_name(name) {
   var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-  if (match) 
+  if (match)
     return match[2];
   else
     return null;
@@ -16,11 +15,11 @@ export function getUserID() {
   return check_cookie_name("UserID")
 }
 
-export function parseJwt (token) {
+export function parseJwt(token) {
   var base64Url = token.split('.')[1];
   var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
   }).join(''));
 
   return JSON.parse(jsonPayload);
@@ -30,7 +29,7 @@ export function isTokenClient(token) {
   const obj = parseJwt(token);
   if (obj.aud.includes("Client"))
     return true;
-  else 
+  else
     return false;
 }
 
@@ -38,8 +37,7 @@ export function isTokenOrganizer(token) {
   return !isTokenClient(token)
 }
 
-export function LogOut() 
-{
+export function LogOut() {
   if (check_cookie_name("Token")) {
     document.cookie = "Token= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
   }
